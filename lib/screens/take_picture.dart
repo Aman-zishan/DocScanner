43f79392134dc714d3e67bb955972dab8b3c749b
Vivek Sonawane <64553247+sonawane-vivek@users.noreply.github.com>
2020-10-05 20:02:29 +0530

    added-exit-alert-feature (#16)

--- lib/screens/take_picture.dart
+++ lib/screens/take_picture.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+import 'dart:ui';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart' show join;
@@ -47,114 +50,200 @@ class TakePictureScreenState extends State<TakePictureScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // Wait until the controller is initialized before displaying the
-      // camera preview. Use a FutureBuilder to display a loading spinner
-      // until the controller has finished initializing.
-      body: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            // If the Future is complete, display the preview.
-            return CameraPreview(_controller);
-          } else {
-            // Otherwise, display a loading indicator.
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: Container(
-        height: 70.0,
-        width: 70.0,
-        child: FittedBox(
-          child: FloatingActionButton(
-            backgroundColor: Colors.lightBlueAccent,
-
-            child: Icon(
-              Icons.camera_alt,
-              color: Colors.white,
+    return WillPopScope(
+      // ignore: missing_return
+      onWillPop: (){
+        showModalBottomSheet(context: context, builder: (context){
+          return Container(
+            decoration: BoxDecoration(
+                color: Colors.transparent
             ),
-            // Provide an onPressed callback.
-            onPressed: () async {
-              // Take the Picture in a try / catch block. If anything goes wrong,
-              // catch the error.
-              try {
-                // Ensure that the camera is initialized.
+            height: 100,
+            width: double.infinity,
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 5,sigmaY: 5),
+              child: Container(
+                height: 100,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(25),
+                        topRight: Radius.circular(25)
+                    ),
+                    color: Colors.white
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(22, 20, 10, 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
 
-                await _initializeControllerFuture;
-
-                // Construct the path where the image should be saved using the
-                // pattern package.
-                final path = join(
-                  // Store the picture in the temp directory.
-                  // Find the temp directory using the `path_provider` plugin.
-                  (await getTemporaryDirectory()).path,
-                  '${DateTime.now()}.png',
-                );
-
-                // Attempt to take a picture and log where it's been saved.
-                await _controller.takePicture(path);
-
-                // If the picture was taken, display it on a new screen.
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => GeneratePage(imagePath: path),
-                  ),
-                );
-              } catch (e) {
-                // If an error occurs, log the error to the console.
-                print(e);
-              }
-            },
-            elevation: 2.0,
-          ),
-        ),
-      ),
-
-      // drawer: Drawer(
-      //   // Add a ListView to the drawer. This ensures the user can scroll
-      //   // through the options in the drawer if there isn't enough vertical
-      //   // space to fit everything.
-      //
-      //   elevation: 1.5,
-      //   child: CDrawer(),
-      // ),
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          height: 70,
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Expanded(
-                child: Container(
-                  alignment: Alignment.center,
-                  child: Icon(
-                    Icons.filter_b_and_w,
-                    color: Colors.black,
-                    size: 30,
+                      Text("Do you really want to exit?",
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            decoration: TextDecoration.none
+                        ),
+                      ),
+                      SizedBox(height: 5,),
+                      Row(
+                        children: [
+                          SizedBox(width: 5,),
+                          FlatButton(
+                            onPressed: (){
+                              Navigator.of(context).pop();
+                            },
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(Radius.circular(20))
+                            ),
+                            color: Colors.black,
+                            child: Text(
+                              "Cancel",
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                  letterSpacing: 1
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 25,),
+                          FlatButton(
+                            onPressed: (){
+                              exit(0);
+                            },
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(Radius.circular(20))
+                            ),
+                            color: Colors.white30,
+                            child: Text(
+                              "Yes",
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  letterSpacing: 1,
+                                  color: Colors.black
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
                   ),
                 ),
               ),
-              Expanded(child: SizedBox(width: 20.0)),
-              Expanded(
-                child: Container(
-                  alignment: Alignment.center,
-                  child: Icon(
-                    Icons.filter,
-                    color: Colors.black,
-                    size: 30,
-                  ),
-                ),
+            ),
+          );
+        } );
+      },
+      child: Scaffold(
+        // Wait until the controller is initialized before displaying the
+        // camera preview. Use a FutureBuilder to display a loading spinner
+        // until the controller has finished initializing.
+        body: FutureBuilder<void>(
+          future: _initializeControllerFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              // If the Future is complete, display the preview.
+              return CameraPreview(_controller);
+            } else {
+              // Otherwise, display a loading indicator.
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: Container(
+          height: 70.0,
+          width: 70.0,
+          child: FittedBox(
+            child: FloatingActionButton(
+              backgroundColor: Colors.lightBlueAccent,
+
+              child: Icon(
+                Icons.camera_alt,
+                color: Colors.white,
               ),
-            ],
+              // Provide an onPressed callback.
+              onPressed: () async {
+                // Take the Picture in a try / catch block. If anything goes wrong,
+                // catch the error.
+                try {
+                  // Ensure that the camera is initialized.
+
+                  await _initializeControllerFuture;
+
+                  // Construct the path where the image should be saved using the
+                  // pattern package.
+                  final path = join(
+                    // Store the picture in the temp directory.
+                    // Find the temp directory using the `path_provider` plugin.
+                    (await getTemporaryDirectory()).path,
+                    '${DateTime.now()}.png',
+                  );
+
+                  // Attempt to take a picture and log where it's been saved.
+                  await _controller.takePicture(path);
+
+                  // If the picture was taken, display it on a new screen.
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => GeneratePage(imagePath: path),
+                    ),
+                  );
+                } catch (e) {
+                  // If an error occurs, log the error to the console.
+                  print(e);
+                }
+              },
+              elevation: 2.0,
+            ),
           ),
         ),
-        shape: CircularNotchedRectangle(),
-        notchMargin: 4.0,
-        color: Colors.white,
+
+        // drawer: Drawer(
+        //   // Add a ListView to the drawer. This ensures the user can scroll
+        //   // through the options in the drawer if there isn't enough vertical
+        //   // space to fit everything.
+        //
+        //   elevation: 1.5,
+        //   child: CDrawer(),
+        // ),
+        bottomNavigationBar: BottomAppBar(
+          child: Container(
+            height: 70,
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: Icon(
+                      Icons.filter_b_and_w,
+                      color: Colors.black,
+                      size: 30,
+                    ),
+                  ),
+                ),
+                Expanded(child: SizedBox(width: 20.0)),
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: Icon(
+                      Icons.filter,
+                      color: Colors.black,
+                      size: 30,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          shape: CircularNotchedRectangle(),
+          notchMargin: 4.0,
+          color: Colors.white,
+        ),
       ),
     );
   }
