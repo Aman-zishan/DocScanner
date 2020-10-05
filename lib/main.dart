da7ea6b91b377701a3981385081b8b950e2b3d4c
Aman-zishan <amanzishan.az@gmail.com>
2020-10-05 17:52:17 +0530

    add splash screen :fire:

--- lib/main.dart
+++ lib/main.dart
@@ -20,10 +20,43 @@ Future<void> main() async {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
-      home: TakePictureScreen(
-        // Pass the appropriate camera to the TakePictureScreen widget.
-        camera: firstCamera,
-      ),
+      home: new SplashScreen(),
+      routes: <String, WidgetBuilder>{
+        '/TakePictureScreen': (BuildContext context) =>
+            new TakePictureScreen(camera: firstCamera)
+      },
     ),
   );
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => new _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  startTime() async {
+    var _duration = new Duration(seconds: 2);
+    return new Timer(_duration, navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.of(context).pushReplacementNamed('/TakePictureScreen');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.white,
+      body: new Center(
+        child: new Image.asset('images/splash_logo.png'),
+      ),
+    );
+  }
 }
